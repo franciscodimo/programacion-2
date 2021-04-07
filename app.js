22e82c06d5f097aa5d15a2665565e85f23d5da38
franciscodimo <63122162+franciscodimo@users.noreply.github.com>
2021-04-07 17:18:09 -0300

    modificacion del include

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var productsRouter = require('./routes/products');
+// var productsRouter = require('./routes/products');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/products', productsRouter);
+// app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
