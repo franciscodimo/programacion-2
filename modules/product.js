98b8a83e2f7b647a37a06a7e5dfee29f0d421af5
EstanislaoRon <eron@udesa.edu.ar>
2021-04-23 22:25:40 -0300

    cambio de imagenes a reljoes

--- modules/product.js
+++ modules/product.js
@@ -4,21 +4,21 @@ let products = [
       Valor: "consultar",
       tipo: "electrodomestico",
       descripcion: "usado",
-      imagen: "https://media.revistagq.com/photos/5f8ff42d7828529cb9a7a2af/master/w_1000,c_limit/rolex-datejust-efron-2.jpg"
+      imagen: "https://media.revistagq.com/photos/5f0c447664f52a842c7f9bcb/master/w_1000,c_limit/relojes-rolex-gmt-master.jpg"
 },{
     id:1,
     nombre: "Bulgari",
     Valor: "consultar",
     descripcion:"512gb de ram",
     tipo: "electrodomestico",
-    imagen:"https://d3ugyf2ht6aenh.cloudfront.net/stores/679/075/products/e6b0fd16-4751-4d71-bbbf-b4409a8d5a01_nube-8a30633851a80c4a1216121382626058-1024-1024.jpg"
+    imagen:"https://media.revistagq.com/photos/5f0c44768c6c2847199b38d3/master/w_1000,c_limit/relojes-rolex-datejust.jpg"
 },{
     id:2,
     nombre: "pelota de futbol",
     Valor: "consultar",
     descripcion:"pinchada",
     tipo: "deporte",
-    imagen:"https://redsport.vteximg.com.br/arquivos/ids/1018610-1000-1000/GA025356310.jpg?v=637171943230300000"
+    imagen:"https://media.revistagq.com/photos/5f0c44768c6c2847199b38d5/master/w_1000,c_limit/relojes-rolex-sea-dweller.jpg"
 },
 {
     id:3,
@@ -26,7 +26,7 @@ let products = [
     Valor: "consultar",
     descripcion:"azul",
     tipo: "tecnologia",
-    imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTODqbfwqVZfQZkaq2GOlu0tnyjkrKnbf0S8A&usqp=CAU"
+    imagen:"https://castellsonline.com/wp-content/uploads/2020/09/75-a-80520.jpg"
 },
 {
     id:4,
@@ -34,7 +34,7 @@ let products = [
     Valor:" Consultar",
     descripcion:"Usada por Many Machado",
     tipo:"Deporte",
-    imagen:"https://http2.mlstatic.com/D_NQ_NP_764814-MLA32408553729_102019-V.jpg",
+    imagen:"https://www.kewaybags.com/bolsos-segunda-mano/fotos/rolex-lady-datejust-26mm-1568818734.jpg",
 },
 {
     id:5,
@@ -42,7 +42,7 @@ let products = [
     Valor:"Consultar",
     descripcion:"600 memoria ram",
     tipo:"Tecnologia",
-    imagen:"https://contino.com.mx/contenido_demo/uploads/2020/09/71717-Celular-Huawei-Y5-Neo-Cafe-5.jpg"
+    imagen:"https://cdn.shopify.com/s/files/1/1593/9031/collections/Rolex_1024x.jpg?v=1562760780"
 },
 {
     id:6,
@@ -50,7 +50,7 @@ let products = [
     Valor:"Consultar",
     descripcion:"El que usaba lebron",
     tipo:"Deporte",
-    imagen:"https://d26lpennugtm8s.cloudfront.net/stores/540/303/products/823771-mla26654240908_012018-o-b0b82471bc0cb523fb15566423958209-1024-1024.jpg"
+    imagen:"https://cdn.shopify.com/s/files/1/0250/7004/3229/products/w7_ladies_ss_black_BGT_B_530x@2x.jpg?v=1580252691"
 },
 {
     id:7,
@@ -58,7 +58,7 @@ let products = [
     Valor:"Consultar",
     descripcion:"Ponele tu nombre",
     tipo:"Deporte",
-    imagen:"https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw15155448/products/AD_FM1182/AD_FM1182-1.JPG"
+    imagen:"https://www.collectorsquare.com/images/are/t1/1922t1/1922t10069-rolex-submariner-date-reference-rolex-16610.jpg"
 },
 ]
 module.exports = products;
