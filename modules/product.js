47d13bfb41a0339d30c8497a37ad1fa9c9b6830d
EstanislaoRon <eron@udesa.edu.ar>
2021-04-25 12:16:50 -0300

    Cambio color y nombres, descripciones del producto

--- modules/product.js
+++ modules/product.js
@@ -1,62 +1,62 @@
 let products = [
     { id:0,
-      nombre: "Rolex dorado",
+      nombre: "Odyssey 4",
       Valor: "consultar",
       tipo: "electrodomestico",
-      descripcion: "usado",
+      descripcion: "Llevando el tiempo del mundo",
       imagen: "https://media.revistagq.com/photos/5f0c447664f52a842c7f9bcb/master/w_1000,c_limit/relojes-rolex-gmt-master.jpg"
 },{
     id:1,
-    nombre: "Bulgari",
+    nombre: "GMT-Master ",
     Valor: "consultar",
-    descripcion:"nuevo",
+    descripcion:"Uno con el tiempo",
     tipo: "electrodomestico",
     imagen:"https://media.revistagq.com/photos/5f0c44768c6c2847199b38d3/master/w_1000,c_limit/relojes-rolex-datejust.jpg"
 },{
     id:2,
-    nombre: "pelota de futbol",
+    nombre: "Maverick",
     Valor: "consultar",
-    descripcion:"pinchada",
+    descripcion:"La hora es ahora",
     tipo: "deporte",
     imagen:"https://media.revistagq.com/photos/5f0c44768c6c2847199b38d5/master/w_1000,c_limit/relojes-rolex-sea-dweller.jpg"
 },
 {
     id:3,
-    nombre: "funda de celular",
+    nombre: "Rohades 300",
     Valor: "consultar",
-    descripcion:"azul",
+    descripcion:"Siempre atento al tiempo",
     tipo: "tecnologia",
     imagen:"https://castellsonline.com/wp-content/uploads/2020/09/75-a-80520.jpg"
 },
 {
     id:4,
-    nombre:" Remera Baseball",
+    nombre:"Iron Flame",
     Valor:" Consultar",
-    descripcion:"Usada por Many Machado",
+    descripcion:"La hora de mañana",
     tipo:"Deporte",
     imagen:"https://www.kewaybags.com/bolsos-segunda-mano/fotos/rolex-lady-datejust-26mm-1568818734.jpg",
 },
 {
     id:5,
-    nombre:"Celular Huaweii",
+    nombre:"Submarine 4",
     Valor:"Consultar",
-    descripcion:"600 memoria ram",
+    descripcion:"Water proof",
     tipo:"Tecnologia",
     imagen:"https://cdn.shopify.com/s/files/1/1593/9031/collections/Rolex_1024x.jpg?v=1562760780"
 },
 {
     id:6,
-    nombre:"Aro de basquet",
+    nombre:"Corazón de Diamante",
     Valor:"Consultar",
-    descripcion:"El que usaba lebron",
+    descripcion:"Es hora de brillar",
     tipo:"Deporte",
     imagen:"https://cdn.shopify.com/s/files/1/0250/7004/3229/products/w7_ladies_ss_black_BGT_B_530x@2x.jpg?v=1580252691"
 },
 {
     id:7,
-    nombre:"Remera de River",
+    nombre:"Londinense",
     Valor:"Consultar",
-    descripcion:"Ponele tu nombre",
+    descripcion:"Honoreando a la ciencia",
     tipo:"Deporte",
     imagen:"https://www.collectorsquare.com/images/are/t1/1922t1/1922t10069-rolex-submariner-date-reference-rolex-16610.jpg"
 },
