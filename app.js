d46dc06221bcdcd46b4edc4822feab089c7d752b
franciscodimo <63122162+franciscodimo@users.noreply.github.com>
2021-04-26 13:42:39 -0300

    cambios en el search

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/product', productRouter);
+app.use('/productEdit', productRouter);
 app.use ('/users', userRouter );
 app.use ('/register', registerRouter );
 app.use ('/login', userRouter );
--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,9 +1,12 @@
 var product = require ('../modules/product') 
     let searchController = { 
-        search: function(req,res) {
+        search: function(req, res) {
+          let search = req.query.search
             res.render('searchResults', {
                 product:product,
                 logueado:false,
+                search:search,
+
             })
         }}
         
