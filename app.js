cc0a34ba62289e03649bc1ac1a899b7f9d18dddd
EstanislaoRon <eron@udesa.edu.ar>
2021-05-28 13:47:08 -0300

    .

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var categoriasRouter = require('./routes/categorias');
 var comentariosRouter = require('./routes/comentarios');
 var securityRouter = require('./routes/security');
 
+
 //Q odna////////////
 
 var app = express();
@@ -41,7 +42,7 @@ const publicRoutes = [
 ]
 app.use(function(req, res, next){
   if(req.session.user != undefined){
-    res.locals = req.session.user
+    res.locals.user = req.session.user
     next();
   } else {
     if (!publicRoutes.includes(req.path)){
@@ -50,6 +51,19 @@ app.use(function(req, res, next){
   }
   next();
 });
+
+app.use(function(req, res, next){
+  if(req.cookies.userId != undefined && req.session.user == undefined){
+    db.User.findByPk(req.cookies.userId)
+    .then( user => {
+      req.session.user = user;
+      return next();
+    })
+    .catch( e => { next(createError(e.status)) })
+  } else {
+    next()
+  }
+})
 
 app.use('/', indexRouter);
 app.use('/product', productRouter);
--- controllers/securityController.js
+++ controllers/securityController.js
@@ -1,23 +1,32 @@
 let db = require('../database/models');
+const bcrypt = require('bcryptjs')
 
 let securityController = {
                             login: function (req, res) {
-                                return res.render ('security/login');
+                                return res.render ('security/login', {
+                                    failed: req.query.failed
+                                });
                             },
                             authenticate: function (req, res) {
-                                db.User.findOne({where: {email: req.body.email}})
+                                db.usuarios.findOne({where : {email : req.body.email}})
                                 .then((user)=> {
-                                    if (req.body.password == user.password){
+                                    if (bcrypt.hashSync(req.body.password) == usuarios.password){
                                         req.session.user = user;
+                                        if (req.body.rememberme){
+                                            res.cookie('userId', user.id, {maxAge: 1000 * 60 * 60 * 24 * 365 /*(un anio)*/ }) 
+                                        }
+                                        
+                                        
                                         return res.redirect('/');
                                     }
                                     res.redirect('/login?failed=true');
                                 })
                                 .catch((error) => {
-                                    res.redirect('/login?failed=true');
+                                    throw error
                                 })
                             },
                             register: function(req,res){
+                                req.body.password = bcrypt.hashSync(req.body.password)
                                 if (req.method == 'POST');
                                 db.user.create(req.body)
                         
@@ -26,11 +35,12 @@ let securityController = {
                                 })
                         
                                 .catch((error) => {
-                                    return res.send(error);
+                                   throw error
                                 })  
                             },
                             logout: function(req, res){
                                 req.session.destroy ();
+                                res.clearCookie('userId')
                                 return res.redirect('/logout');
                             }
 }
