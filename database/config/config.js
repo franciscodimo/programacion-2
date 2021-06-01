bbd88ff7865ff49b0055438d21ac3002ab3fa91a
franciscodimo <63122162+franciscodimo@users.noreply.github.com>
2021-06-01 19:40:20 -0300

    arreglo

--- database/config/config.js
+++ database/config/config.js
@@ -3,11 +3,7 @@ module.exports =
   "development": {
     "username": "root",
     "password": null,
-<<<<<<< HEAD
     "database": "integrador",
-=======
-    "database": "Integrador",
->>>>>>> af75d2a235fe637fad6d8dcf3a737342243d78d9
     "host": "127.0.0.1",
     "dialect": "mysql"
   }}
