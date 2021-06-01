e6d96226b434217b2c91b26ae28a53bdc195b588
franciscodimo <63122162+franciscodimo@users.noreply.github.com>
2021-06-01 14:05:12 -0300

    product controller

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,18 +1,23 @@
 let db = require("../database/models");
 let productController = { 
 	product: function (req, res) {
-            
 		var id =  req.params.id
 		db.productos.findByPk(id)
-		.then((data) => {
-			return res.render('product', { 
-				product: data 
-			});
+		.then((producto) => {
+			db.comentarios.findAll({
+				where: {producto_id: producto.id},
+				include: [{association: "usuario"}]
+			})
+			.then((comentarios) => {
+				return res.render('product', { 
+					product: producto,
+					comentarios: comentarios
+				});
+			})
 		})
-
-	.catch((error) => {
-		return res.send(error);
-	})
+		.catch((error) => {
+			return res.send(error);
+		})
 	}}
 
 
