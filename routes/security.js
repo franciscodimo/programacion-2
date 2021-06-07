d955de03217298b7ba08b220310fa9cef3eebadb
franciscodimo <63122162+franciscodimo@users.noreply.github.com>
2021-06-07 16:52:53 -0300

    multer de usuario

--- routes/security.js
+++ routes/security.js
@@ -1,13 +1,22 @@
 var express = require ('express');
 var router = express.Router();
-
+const multer = require ('multer');
+const path = require ('path');
 const sequrityController = require ('../controllers/securityController');
 // const { route } = require('./productAdd');
-
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images');
+    },
+    filename: (req, file, cb)=>{
+        cb(null, Date.now() + '-' + file.filename + path.extname(file.originalname))
+    }
+})  
+const upload = multer({storage: storage})
 router.get('/login', sequrityController.login);
 router.post('/login', sequrityController.authenticate);
 router.get('/register', sequrityController.register);
-router.post('/register', sequrityController.register);
+router.post('/register', upload.single('url_imagen'), sequrityController.register);
 router.all('/logout', sequrityController.logout);
 
 module.exports = router;
