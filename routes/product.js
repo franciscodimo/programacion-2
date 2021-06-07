defd328a24258723c443c6b3ee3e7583c2cf6c2e
franciscodimo <63122162+franciscodimo@users.noreply.github.com>
2021-06-07 15:26:51 -0300

    multer y archivos

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const multer = require ('multer');
+const path = require ('path');
 const productController = require('../controllers/productControllers');
 router.get('/:id', productController.product);
-// router.get('/', productController.form);
-// router.post('/', productController.create);
+router.get('/', productController.form);
+router.post('/', productController.create);
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images');
+    },
+    filename: (req, file, cb)=>{
+        cb(null, file.filename + '' + Date.now() + path.extname(file.originalname))
+    }
+})  
+const upload = multer({storage: storage})
 
 module.exports = router; 
