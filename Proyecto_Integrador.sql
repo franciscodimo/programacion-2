ac4cf8989f15076b8e43f2f2d23e7466d0fa0c7a
EstanislaoRon <eron@udesa.edu.ar>
2021-06-10 17:05:50 -0300

    .

--- Proyecto_Integrador.sql
+++ Proyecto_Integrador.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 10, 2021 at 09:54 PM
+-- Generation Time: Jun 10, 2021 at 10:04 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -20,6 +20,133 @@ SET time_zone = "+00:00";
 --
 -- Database: `integrador`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `primary` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`, `primary`) VALUES
+(1, 'Mens Watches', NULL),
+(2, 'Luxury Watches', 1),
+(3, 'Rolex', 2),
+(4, 'Tag Heuer', 2),
+(5, 'Omega', 2),
+(6, 'Daytona', 3),
+(7, 'Date-Just', 3),
+(8, 'Day-Date', 3),
+(9, 'Submariner', 3),
+(10, 'Carrera', 4),
+(11, 'Aquar\r\nacer', 4),
+(12, 'Speedmaster', 5),
+(13, 'Seamaster', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `producto_id` int(11) NOT NULL,
+  `texto_comentario` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `usuario_id`, `producto_id`, `texto_comentario`, `created_at`) VALUES
+(1, 1, 15, 'Love this watch, great for everyday use, sports and even fancy dinners! Can\'t go wrong with the Tag Heuer Aquaracer! Highly recommended.', '2021-06-02 22:37:50'),
+(2, 1, 19, 'Words can\'t describe what this watch is in person, just unbelievable!', '2021-06-02 22:37:50'),
+(3, 1, 16, 'Great Watch for sports and outdoor life. Liked it so much I\'m looking to get the one in black for fancier events.', '2021-06-02 22:37:50'),
+(4, 1, 21, 'Love the watch and had a super good experience buying it at Relox.', '2021-06-02 22:37:50'),
+(5, 1, 3, 'I mean, it\'s definitely a Rolex, cant go wrong with them swiss.', '2021-06-02 22:37:50'),
+(6, 1, 21, 'Beautiful watch, bought it for my husband for our 10 year anniversary!', '2021-06-02 22:37:50'),
+(7, 1, 20, 'Highly recommendable watch! Super versatile and great for fine-dining.', '2021-06-02 22:37:50'),
+(8, 1, 20, 'Had my eye on this one for a long time. Great watch, super versatile and wearable.', '2021-06-02 22:37:50'),
+(9, 2, 3, 'Love the watch and had a super good experience buying it at Relox.', '2021-06-02 22:37:50'),
+(10, 2, 20, 'Words can\'t describe what this watch is in person, just unbelievable!', '2021-06-02 22:37:50'),
+(11, 2, 10, 'Can\'t be happier with my new watch! However, Reloxs site responsiveness could be improved.', '2021-06-02 22:37:50'),
+(12, 2, 16, 'Highly recommendable watch! Super versatile and great for fine-dining.', '2021-06-02 22:37:50'),
+(13, 2, 17, 'Love the watch and had a super good experience buying it at Relox.', '2021-06-02 22:37:50'),
+(14, 2, 17, 'Beautiful watch, bought it for my husband for our 10 year anniversary!', '2021-06-02 22:37:50'),
+(15, 2, 17, 'Words can\'t describe what this watch is in person, just unbelievable!', '2021-06-02 22:37:50'),
+(16, 2, 10, 'Great Watch for sports and outdoor life. Liked it so much I\'m looking to get the one in black for fancier events.', '2021-06-02 22:37:50'),
+(17, 5, 18, 'Love the watch and had a super good experience buying it at Relox.', '2021-06-02 22:37:50'),
+(18, 5, 6, 'Highly recommendable watch! Super versatile and great for fine-dining.', '2021-06-02 22:37:50'),
+(19, 5, 21, 'Great Watch for sports and outdoor life. Liked it so much I\'m looking to get the one in black for fancier events.', '2021-06-02 22:37:50'),
+(20, 5, 19, 'Words can\'t describe what this watch is in person, just unbelievable!', '2021-06-02 22:37:50'),
+(21, 5, 21, 'Beautiful watch, bought it for my husband for our 10 year anniversary!', '2021-06-02 22:37:50'),
+(22, 5, 21, 'Highly recommendable watch! Super versatile and great for fine-dining.', '2021-06-02 22:37:50'),
+(23, 5, 6, 'Great Watch for sports and outdoor life. Liked it so much I\'m looking to get the one in black for fancier events.', '2021-06-02 22:37:50'),
+(24, 5, 21, 'Beautiful watch, bought it for my husband for our 10 year anniversary!', '2021-06-02 22:37:50'),
+(25, 6, 19, 'Can\'t be happier with my new watch! However, Reloxs site responsiveness could be improved.', '2021-06-02 22:37:50'),
+(26, 6, 16, 'Absolutely astonished by the watch itself. It\'s the perfect all-around watch for everyday use. Feel it\'s gonna age beautifully with wear and tear.', '2021-06-02 22:37:50'),
+(27, 6, 6, 'Beautiful watch, bought it for my husband for our 10 year anniversary!', '2021-06-02 22:37:50'),
+(28, 6, 21, 'Can\'t be happier with my new watch! However, Reloxs site responsiveness could be improved.', '2021-06-02 22:37:50'),
+(29, 6, 3, 'Absolutely astonished by the watch itself. It\'s the perfect all-around watch for everyday use. Feel it\'s gonna age beautifully with wear and tear.', '2021-06-02 22:37:50'),
+(30, 6, 16, 'Love the watch and had a super good experience buying it at Relox.', '2021-06-02 22:37:50'),
+(31, 6, 17, 'Highly recommendable watch! Super versatile and great for fine-dining.', '2021-06-02 22:37:50'),
+(32, 6, 20, 'Beautiful watch, bought it for my husband for our 10 year anniversary!', '2021-06-02 22:37:50'),
+(33, 7, 6, 'Can\'t be happier with my new watch! However, Reloxs site responsiveness could be improved.', '2021-06-02 22:37:50'),
+(34, 7, 21, 'Great Watch for sports and outdoor life. Liked it so much I\'m looking to get the one in black for fancier events.', '2021-06-02 22:37:50'),
+(35, 7, 15, 'Absolutely astonished by the watch itself. It\'s the perfect all-around watch for everyday use. Feel it\'s gonna age beautifully with wear and tear.', '2021-06-02 22:37:50'),
+(36, 7, 16, 'Can\'t be happier with my new watch! However, Reloxs site responsiveness could be improved.', '2021-06-02 22:37:50'),
+(37, 7, 10, 'Beautiful watch, bought it for my husband for our 10 year anniversary!', '2021-06-02 22:37:50'),
+(38, 7, 3, 'Great Watch for sports and outdoor life. Liked it so much I\'m looking to get the one in black for fancier events.', '2021-06-02 22:37:50'),
+(39, 7, 17, 'Great Watch for sports and outdoor life. Liked it so much I\'m looking to get the one in black for fancier events.', '2021-06-02 22:37:50'),
+(40, 7, 3, 'Great Watch for sports and outdoor life. Liked it so much I\'m looking to get the one in black for fancier events.', '2021-06-02 22:37:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `categoria_id` int(11) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `url_imagen` varchar(255) NOT NULL,
+  `descripcion` varchar(300) NOT NULL,
+  `precio` float DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `productos`
+--
+
+INSERT INTO `productos` (`id`, `categoria_id`, `usuario_id`, `nombre`, `url_imagen`, `descripcion`, `precio`, `created_at`) VALUES
+(3, 4, 8, 'Tag Heuer Carrera Automatic', 'relojregalo.png', 'El mejor reloj del mundo\r\n', 120, '2021-06-02 00:23:57'),
+(6, 5, 8, 'Omega Speedmaster Special Edition', 'basededatos6.png', '', 35, '2021-06-02 00:23:57'),
+(10, 4, 8, 'Tag Heuer Aquaracer', 'elonce.jpg', '', 200, '2021-06-02 00:23:57'),
+(15, 5, 9, 'Omega Seamaster 007', 'R81311e7d95377db7d475187a8b393b8f.jpg', '', 300, '2021-06-02 00:23:57'),
+(16, 5, 9, 'Omega Speedmaster Moonwatch', 'base de datos.jpg', '', 350, '2021-06-02 00:23:57'),
+(17, 3, 9, 'Rolex Daytona Chronograph', 'R6316c7a0c712140062446c05f2a3cdf9.png', '', 560, '2021-06-02 00:23:57'),
+(18, 3, 9, 'Rolex Submariner', 'fotorelojgris.jpg', '', 470, '2021-06-02 00:23:57'),
+(19, 3, 10, 'Rolex Date-Just 39', 'queondasifunciona.jpg', '', 460, '2021-06-02 00:23:57'),
+(20, 3, 11, 'Rolex Day-Date President', 'todooro.png', '', 670, '2021-06-02 00:23:57'),
+(21, 3, 11, 'Rolex Date-just 41', 'OIP.jpg', '', 560, '2021-06-02 00:23:57'),
+(22, 3, 14, 'Venom', 'reloj9.png', '', 3000, '2021-06-02 00:23:57'),
+(23, 6, 5, 'Onix', 'verdoso.png', '', 4000, '2021-06-02 00:23:57'),
+(242, 11, 20, 'Rolex 300 mega', '1623111431656-undefined.jpg', 'The best watch ever', NULL, '2021-06-08 00:17:11');
 
 -- --------------------------------------------------------
 
@@ -65,6 +192,28 @@ INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `fecha_de_nacimiento`, `emai
 --
 
 --
+-- Indexes for table `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `producto_id` (`producto_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indexes for table `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria_id` (`categoria_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
 -- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -75,10 +224,46 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT for table `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=291;
+
+--
+-- AUTO_INCREMENT for table `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=244;
+
+--
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`),
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Constraints for table `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`),
+  ADD CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
