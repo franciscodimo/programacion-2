537f228220ba6d27b4f8adb410bd2b14741e77fc
franciscodimo <63122162+franciscodimo@users.noreply.github.com>
2021-06-21 17:41:22 -0300

    crash con commit

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,5 +1,6 @@
 let db = require("../database/models");
 let profileController = {
+<<<<<<< HEAD
    profile:  async function (req, res) {
     let productos = await db.productos.findAll({
       where: { usuario_id: req.params.id },
@@ -21,6 +22,33 @@ let profileController = {
     res.render('profile-edit')
   }, 
  
+=======
+    profile: function(req, res) {
+      let productos = db.productos.findByPk({
+        where: { 
+          usuario_id: req.session.user.id,
+        }    
+          
+      })
+        res.render('profile' ,{
+            productos:productos,
+            product: data
+        } )
+        
+    },
+    edit: (req, res) => {
+      let usuarios =  db.usuarios.findByPk(req.params.id)
+      .then((data) => {
+          res.render('profile-edit' ,{
+              usuario: data,
+          } )
+      })
+      .catch((error) => {
+          return res.send(error);
+      })
+      
+  },
+>>>>>>> 59d924fc7314b36fd5e36135f4ecd56b0be69d4d
 }
 
 module.exports = profileController;
