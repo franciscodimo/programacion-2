b96cef638dc45c41458bfe821ae71eb615cf4ebd
franciscodimo <63122162+franciscodimo@users.noreply.github.com>
2021-06-23 18:07:02 -0300

    external image, arreglo de estilo

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,6 @@ const productController = require('../controllers/productControllers');
 var router = express.Router();
 let profileController = require('../controllers/profileController');
 router.get('/edit',profileController.profileEdit);
-router.post("/edit", profileController.profileEdit)
+router.post('/edit', profileController.profileEdit)
 router.get('/:id',profileController.profile);
 module.exports = router;
