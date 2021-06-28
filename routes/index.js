0cb9dbc5ca3234cf26ee8c74a9cbe06ae6aa5ea2
EstanislaoRon <eron@udesa.edu.ar>
2021-06-28 12:14:36 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,3 @@ router.get('/mapa', indexController.mapa);
 
 module.exports = router;
 
-// pasar register a controladores.
-// agregar metodo register a usercontroller y vista register
-// en la vista de register debe dar un formulario 
-// hacer todo de login (formulario)
-// modificar el navegador 
-// corregir lo de mas comentados (repetir el mismo for en los mas comentados)
-// hacer todo sobre agregar producto (un formulario que tiene que tener los mismos campos que products ejs)
-// tiene que verse el boton agregar producto, solo si esta logueado
--- routes/product.js
+++ routes/product.js
@@ -5,20 +5,19 @@ const path = require ('path');
 const productController = require('../controllers/productControllers');
 
 var storage = multer.diskStorage({
-    destination: (req, file, cb) => { //metodo //definimos la ruta donde se va a almacenar el archivo
-        cb(null, './public'); //en esta linea le debemos indicar una ruta que representa donde se va a guardar el archivo y el null es como error
+    destination: (req, file, cb) => { 
+        cb(null, './public'); 
     },
-    filename: (req, file, cb)=>{ //metodo //indicamos el nombre con el que se guardara el archivo
-        cb(null, Date.now() + '-' + file.fieldname + path.extname(file.originalname)) // con date.now es el momento exacto en el que se sube el archivo, el file.fieldname sirve para dar el nombre del archivo y por ultimo el .jpg de la extension
+    filename: (req, file, cb)=>{ 
+        cb(null, Date.now() + '-' + file.fieldname + path.extname(file.originalname)) 
     }
 })  
-const upload = multer({storage: storage}) //en esta lineaa lo que hacemos es almacenar el archivo con un objeto con la variable storage que fue nombrada previamente 
+const upload = multer({storage: storage}) 
 
 router.get('/:id', productController.product);
 router.get('/', productController.form);
-router.post('/', upload.single('url_imagen'), productController.create); //aca se le pasa el upload.single para cargar la imagen y el nombre del input del formulario donde tengo el file
+router.post('/', upload.single('url_imagen'), productController.create); 
 router.post('/:id/comentar', productController.comentarios);
 router.post('/', productController.add);
 router.all("/:id/delete", productController.eliminarProducto);
 module.exports = router; 
- //todo este desarrollo de multer aca forma una especie de middlewere, este middlewere es algo que se ejecuta siempre y tiene la carecteristica de ir devolviendo y migrando los distintos tipos de request y response
